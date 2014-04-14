2a77f075600ebb7edcd53216374651c2a9dcf5fe
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-04-14 18:32:06 +0900

    update

---
+++ Metal_Wolf_Chaos.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:Metal_Wolf_Chaos) do
+  command(:metal_wolf_chaos,
+          name: '大統領',
+          condition:Plugin::Command[:Editable],
+          visible:true,
+          role: :postbox) do |opt|
+    tweet(Service.primary,"How do you like me now")
+  end
+  
+  def tweet(serv,texts)
+    serv.update(:message => texts)
+  end
+
+end
---
+++ spec
@@ -0,0 +1,9 @@
+---
+slug: :Metal_Wolf_Chaos
+depends:
+  mikutter: 0.2.2.1537
+  plugin: []
+version: '1.0'
+author: masayoshi_main
+name: Metal Wolf Chaos
+description: 某言い回しが中二病な大統領に誰でもなれる!
